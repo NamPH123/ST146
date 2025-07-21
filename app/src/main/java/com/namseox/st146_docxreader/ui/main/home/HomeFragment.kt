784d5ede4ec819563c65792fa4b550af62dbca0b
namseox <namseox@gmail.com>
2025-07-21 18:49:35 +0700

    first commit

--- app/src/main/java/com/namseox/st146_docxreader/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/namseox/st146_docxreader/ui/main/home/HomeFragment.kt
@@ -1,6 +1,10 @@
 package com.namseox.st146_docxreader.ui.main.home
 
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.PopupWindow
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -8,6 +12,7 @@ import androidx.lifecycle.repeatOnLifecycle
 import com.countryhuman.countryball.maker.base.AbsBaseFragment
 import com.namseox.st146_docxreader.R
 import com.namseox.st146_docxreader.databinding.FragmentHomeBinding
+import com.namseox.st146_docxreader.databinding.PopupSortBinding
 import com.namseox.st146_docxreader.dialog.DialogNewDocument
 import com.namseox.st146_docxreader.model.UiState
 import com.namseox.st146_docxreader.ui.main.AllFilesAdapter
@@ -15,6 +20,7 @@ import com.namseox.st146_docxreader.ui.main.MainActivity
 import com.namseox.st146_docxreader.ui.main.MainViewModel
 import com.namseox.st146_docxreader.utils.dataFloderCache
 import com.namseox.st146_docxreader.utils.dataListFile
+import com.namseox.st146_docxreader.utils.dpToPx
 import com.namseox.st146_docxreader.utils.onSingleClick
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -62,9 +68,53 @@ class HomeFragment : AbsBaseFragment<FragmentHomeBinding, MainActivity>() {
             }
         }
     }
+    lateinit var bindingPopup: PopupSortBinding
+    lateinit var popup: PopupWindow
+    fun changeBGPopupSort(i: Int) {
+        bindingPopup.llName.setBackgroundColor("#ffffff".toInt())
+        bindingPopup.llSize.setBackgroundColor("#ffffff".toInt())
+        bindingPopup.llDate.setBackgroundColor("#ffffff".toInt())
+        bindingPopup.llType.setBackgroundColor("#ffffff".toInt())
+        when (i) {
+            0 -> {
+                bindingPopup.llName.setBackgroundColor("#F4F7FF".toInt())
+            }
 
+            1 -> {
+                bindingPopup.llSize.setBackgroundColor("#F4F7FF".toInt())
+            }
+
+            2 -> {
+                bindingPopup.llDate.setBackgroundColor("#F4F7FF".toInt())
+            }
+
+            3 -> {
+                bindingPopup.llType.setBackgroundColor("#F4F7FF".toInt())
+            }
+        }
+    }
     override fun setClick() {
+        bindingPopup = PopupSortBinding.inflate(LayoutInflater.from(requireContext()))
+        popup = PopupWindow(
+            bindingPopup.root,
+            dpToPx(162f, requireContext()).toInt(),
+            WRAP_CONTENT,
+            true
+        )
+        bindingPopup.llName.onSingleClick { changeBGPopupSort(0) }
+        bindingPopup.llSize.onSingleClick { changeBGPopupSort(1) }
+        bindingPopup.llDate.onSingleClick { changeBGPopupSort(2) }
+        bindingPopup.llType.onSingleClick { changeBGPopupSort(3) }
         binding.apply {
+            imvSort.setOnClickListener {
+                popup.showAsDropDown(
+                    it,
+                    -(dpToPx(140f, requireContext()).toInt()),
+                    -(dpToPx(0f, requireContext()).toInt()),
+                    Gravity.CENTER
+                )
+
+            }
             imvPlus.onSingleClick {
                 var dialogNewDocument = DialogNewDocument(requireActivity())
                 dialogNewDocument.onClick = {
